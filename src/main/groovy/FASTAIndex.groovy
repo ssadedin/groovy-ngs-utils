25554192c581e5581d83691281415ed3585b5aa2
Simon Sadedin <simon.sadedin@mcri.edu.au>
2014-03-23 09:25:00 +1100

    major cleanup, remove hard coded limits, replace with attributes, support for max rows option

--- src/main/groovy/FASTAIndex.groovy
+++ src/main/groovy/FASTAIndex.groovy
@@ -23,22 +23,57 @@ import net.sf.samtools.util.SequenceUtil;
 
 /**
  * A very simplistic FASTA index that allows lookup of sequence names by their
- * sequences. Currently indexes the first 30 base pairs of each sequence,
- * and that is all!
+ * sequence content using a fixed length prefix seed. It only supports looking up
+ * sequences by their prefixes, with an optional offset
+ * from the query start by up to specified number of bases (default=5). Both 
+ * the original sequence AND its reverse complement are indexed, so you can
+ * perform a single query to identify a sequence in the case you are not sure
+ * what the strand / orientation of the query sequence is.
+ * <p>
+ * NOTE: this class is intended for indexing large numbers of SHORT sequences.
+ * It will not work for indexing, for example, a reference sequence for an 
+ * organism! (You will be able to look up each chromosome by a short prefix, not
+ * terribly useful).
+ * <p>
+ * NOTE2: this class is not for working at low level with pre-indexed FASTA files 
+ * (eg: .fai format) For working with those, use the {@link FASTA} class. This class
+ * takes a {@link FASTA} object and adds ability to look up by sequence content
+ * to it.
+ * <p>Example:
+ * <pre>
+ * index = new FASTAIndex(new FASTA("tests/test.fasta"), 0..20)
+ * assert index.querySequence("AGTCCCTATTACAAA") == "amplicon_1"
+ * </pre>
  * 
  * @author simon.sadedin@mcri.edu.au
  */
 class FASTAIndex {
 
     /**
-     * Index of amplicon names maps name => sequence
+     * Index of amplicon names maps name => full sequence
      */
-    Map sequenceNames = new HashMap(40000)
+    Map<String,String> sequenceNames = new HashMap(40000)
     
     /**
      * Index of amplicon sequences, maps subsequence to amplicon name(s)
      */
-    Map sequences = new HashMap(40000)
+    Map<String,String> sequences = new HashMap(40000)
+    
+    /**
+     * Range of offsets from beginning of sequences to index
+     * (memory expensive to increase this a lot)
+     */
+    IntRange offsetRange = 0..6
+    
+    /**
+     * Maximum number of sequences to index (0 means unlimited)
+     */
+    int maxSize = 0 
+    
+    /**
+     * Size of seed to use (bp)
+     */
+    int seedSize = 30
     
     /**
      * Create an index from the given fasta, where each fasta sequence corresponds
@@ -46,28 +81,52 @@ class FASTAIndex {
      * 
      * @param fasta
      */
-    public FASTAIndex(FASTA fasta, BED bed=null) {
+    public FASTAIndex(FASTA fasta, BED bed=null) {    
+        this.index(fasta,bed)
+    }
+    
+    /**
+     * Index the given fasta, masked using the given BED file
+     * @param fasta
+     * @param bed
+     */
+    void index(FASTA fasta, BED bed) {
         ProgressCounter counter = new ProgressCounter()
+        
+        int countTooShort = 0
         
         def handler = { String amplicon, byte[] bases ->
             
             // Ignore stupidly short sequences
-            if(bases.size()<40)
+            if(bases.size()<seedSize+offsetRange.to+1) {
+                ++countTooShort
                 return
+            }
+            
+            boolean log = false
+//            if(amplicon=="chr1:154926092-154926234") {
+//                log=true
+//            }
             
             sequenceNames[amplicon] = new String(bases).toUpperCase()
             
-            for(i in 0..6) {
-              String sequence = new String(bases,i,30).toUpperCase()
+            for(i in offsetRange.from..(offsetRange.to+1)) {
+              String sequence = new String(bases,i,seedSize).toUpperCase()
+              if(log)
+                  println "Indexing $amplicon: $sequence"
               sequences[sequence] = amplicon
             }
             
             SequenceUtil.reverseComplement(bases)
-            for(i in 0..6) {
-              String sequence = new String(bases,i,30).toUpperCase()
+            for(i in offsetRange.from..(offsetRange.to+1)) {
+              String sequence = new String(bases,i,seedSize).toUpperCase()
               sequences[sequence] = amplicon
             }
+            
             counter.count()
+            
+            if(maxSize > 0 && counter.count >= maxSize) 
+                throw new Abort()
         }
         
         if(bed) {
@@ -76,5 +135,28 @@ class FASTAIndex {
         else {
            fasta.eachSequence(handler)
         }
+        
+        if(countTooShort * 100 / counter.count > 10) {
+            throw new IllegalStateException("More than 10% of your sequences were shorter than the seed length. Please use a shorter seed length or check that your data is correct.")
+        }
+    }
+    
+    String querySequenceName(String sequence) {
+        if(sequence.size()<seedSize) 
+            System.err.println("A sequence queried ($sequence) was shorter than the seed length ($seedSize). Querying sequences shorter than the seed size is not supported.")
+        return sequences[sequence.substring(0, seedSize)]
+    }
+    
+    /**
+     * Create an index from the given fasta, where each fasta sequence corresponds
+     * to a single amplicon
+     * 
+     * @param fasta
+     */
+    public FASTAIndex(FASTA fasta, IntRange offsetRange, int maxSize=0, int seedSize=30, BED bed=null) {
+        this.maxSize = maxSize
+        this.seedSize = seedSize
+        this.offsetRange = offsetRange
+        this.index(fasta,bed)
     }
 }
