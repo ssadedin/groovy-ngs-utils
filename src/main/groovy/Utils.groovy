eede96652bcc48a17eb278d47b4b0cbe53a8209f
Simon Sadedin <ssadedin@gmail.com>
2014-09-08 15:04:45 +1000

    conveniene utils for dealing with int arrays, avoid groovy overhead

--- src/main/groovy/Utils.groovy
+++ src/main/groovy/Utils.groovy
@@ -1,5 +1,6 @@
 import groovy.lang.Closure;
 import groovy.time.TimeCategory;
+import groovy.transform.CompileStatic;
 
 
 /**
@@ -20,5 +21,30 @@ class Utils {
             System.err.println(("$desc executed in " + TimeCategory.minus(endTime,startTime)).center(80,"="))
         }
         // return endTime.time - startTime.time
+    }
+
+	@CompileStatic
+	static int[] array(int... values) {
+		return values
+	}
+    
+    @CompileStatic
+	static int max(int[] values) {
+		int max = Integer.MIN_VALUE;
+		for(int value : values) {
+				if(value > max)
+						max = value;
+		}
+		return max;
+    }
+    
+    @CompileStatic
+	static int min(int[] values) {
+		int min = Integer.MAX_VALUE;
+		for(int value : values) {
+				if(value < min)
+						min = value;
+		}
+		return min;
     }
 }
