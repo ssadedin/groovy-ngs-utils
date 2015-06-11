8596ab5f21d6d2bd8c68e3bbc12225ae10acf348
Simon Sadedin <ssadedin@gmail.com>
2015-06-11 21:12:02 +1000

    support to read only selected regions from a coverage file, support for coverage files without id column

--- src/main/groovy/BEDToolsCoverage.groovy
+++ src/main/groovy/BEDToolsCoverage.groovy
@@ -1,23 +1,53 @@
+import graxxia.*
 
 class BEDToolsCoverage {
     
     Regions covs = new Regions()
     
+    IntegerStats allStats = new IntegerStats(1000)
+    
+    IntegerStats inStats = new IntegerStats(1000)
+    
     BEDToolsCoverage(String file) {
-        TSV covFile = new TSV(file, columnNames:["chr","start","end","gene","offset","cov"], readFirstLine:true)
+        this(file,null)
+    }
+    
+    BEDToolsCoverage(String file, Regions mask) {
+        
+        // Sniff the file to see if there are 5 columns or 6 - if the original bed file had an id 
+        // column then the BedTools output also has it
+        def firstLine = new File(file).newReader().readLine().split('\t')
+        
+        List<String> cols = ["chr","start","end","offset","cov"]
+        boolean hasId = false
+        if(firstLine.size()>5) {
+            hasId = true
+            cols = ["chr","start","end","id","offset","cov"]
+        }
+        
+        TSV covFile = new TSV(file, columnNames:cols, readFirstLine:true)
         Region r = null
         List regionCovs = []
+        boolean inMask = false
         for(cov in covFile) {
            if(r == null || r.chr != cov.chr || r.from != cov.start || r.to != cov.end) { // new region
-               if(r != null) // add previous region
+               if((r != null) && inMask) // add previous region
                    covs.addRegion(r)
                r = new Region(cov.chr, cov.start..cov.end)
                r.covs = []
-               r.gene = cov.gene
+               
+               inMask = (mask == null || r.overlaps(mask))
+               
+               if(hasId)
+                   r.id = cov.id
            }
+           if(inMask)
+               inStats.addValue(cov.cov)
+           allStats.addValue(cov.cov)    
            r.covs.add(cov.cov)
+           
         }
-        if(r != null) // add previous region
+        if((r != null) && inMask) // add previous region
             covs.addRegion(r)
     }
     
