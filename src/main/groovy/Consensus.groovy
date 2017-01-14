61cf84ac6c069e80e164d461911d9f8d06328bb2
Simon Sadedin <ssadedin@gmail.com>
2017-01-14 16:00:35 -0500

    add utility to calculate consensus between different seqs

---
+++ src/main/groovy/Consensus.groovy
@@ -0,0 +1,116 @@
+import groovy.transform.CompileStatic;
+
+/**
+ * Stores a set of base counts for a base read at a given position,
+ * and supports scoring metrics.
+ * 
+ * @author simon
+ */
+@CompileStatic
+class BaseCounts {
+    
+    BaseCounts(List<String> seqs, int pos) {
+      for(int j=0;j<seqs.size();++j) {
+        String seq = seqs[j]
+        if(seq.size()<=pos)
+            continue
+        switch(seqs[j][pos]) {
+          case 'A': ++a; break;
+          case 'C': ++c; break;
+          case 'T': ++t; break;
+          case 'G': ++g; break;
+          default: ++n;
+        }
+      }
+    }
+    
+    int a=0,t=0,c=0,g=0,n=0
+    
+    String toString() { "cons: ${getConsensus()}, a:$a, t:$t, c:$c, g:$g, n:$n, score: ${getScore()}"  }
+    
+    char consensus = 'n'
+    
+    double getScore() {
+      if(score < 0)
+        score()
+      return score
+    }
+    
+    char getConsensus() {
+      if(score < 0)
+        score()
+      return consensus
+    }
+     
+    double score = -1.0d
+    
+    double score() {
+      
+      double total = Math.max(a+c+t+g,2) -1
+      
+      if(a>=t && a>=c && a>=g) {
+        consensus='A'
+        score = (a-1) / total
+      }
+      else
+      if(c>=t && c>=g) {
+        score = (c-1) / total
+        consensus='C'
+      }
+      else
+      if(g>=t) {
+        score = (g-1) / total
+        consensus='G'
+      }
+      else {
+        consensus='T'
+        score = (t-1) / total
+      }
+    }
+    
+    double bestScore() {
+      Math.max(Math.max(Math.max(a,t),c),g)/(a+t+c+g)
+    }
+}
+
+/**
+ * Support to determine a consensus sequence from a set of sequences
+ * 
+ * @author simon
+ */
+@CompileStatic
+class Consensus {
+    
+    BaseCounts [] baseCounts
+    
+    List<String> seqs
+    
+    int scanMax
+    
+    Consensus(List<String> sequences, int scanMax=Integer.MAX_VALUE) {
+        
+        this.seqs = sequences
+        
+        this.scanMax = Math.min(scanMax, seqs*.size().max())
+        
+        baseCounts = new BaseCounts[sequences.max { it.size() }.size()]
+    }
+    
+    String bases = null
+    
+    double score = 0.0d
+    
+    Consensus build() {
+        StringBuilder cons = new StringBuilder()
+        for(int i=0; i<scanMax; ++i) {
+          BaseCounts bc = new BaseCounts(seqs, i)
+//          println "Base counts at $i: $bc"
+          
+          cons.append(bc.getConsensus())
+          score += bc.score
+        }
+        bases = cons.toString()
+        return this
+    }
+
+}
---
+++ src/test/groovy/ConsensusTest.groovy
@@ -0,0 +1,132 @@
+/*
+ *  Groovy NGS Utils - Some simple utilites for processing Next Generation Sequencing data.
+ *
+ *  Copyright (C) 2016 Simon Sadedin, ssadedin<at>gmail.com
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+class ConsensusTest {
+    
+    @Test
+    void testOneSequence() {
+        
+        def cons = new Consensus(["ATCGCGA"])
+        cons.build()
+        assert cons.bases == "ATCGCGA"
+        assert cons.score == 0.0d
+    }
+    
+    @Test
+    void testOneDifference() {
+        def cons = new Consensus([
+            "ATCGCGA",
+            "ATCGCGA",
+            "ATCGCGT",
+        ])
+        cons.build()
+        assert cons.bases == "ATCGCGA"
+        assert cons.score > 6.0d  && cons.score < 7.0d
+    }
+
+    @Test
+    void testTwoDifferences() {
+        def cons = new Consensus([
+            "ATCGCGA",
+            "ATCGCGA",
+            "ATTGCGA",
+            "ATCGCGT",
+        ])
+        cons.build()
+        assert cons.bases == "ATCGCGA"
+        assert cons.score > 6.0d  && cons.score < 7.0d
+    }
+    
+    @Test
+    void testTwoDifferencesSameBase() {
+        def cons = new Consensus([
+            "ATCGCGA",
+            "ATCGCGA",
+            "ATCGCGT",
+            "ATCGCGT",
+        ])
+        cons.build()
+        assert cons.bases == "ATCGCGA"
+        assert cons.score > 6.0d  && cons.score < 6.4d
+    }
+    
+    
+    @Test
+    void testOppositeOrder() {
+        def cons = new Consensus([
+            "ATCGCGA",
+            "ATCGCGA",
+            "ATTGCGA",
+            "ATCGCGT",
+        ].reverse())
+        cons.build()
+        assert cons.bases == "ATCGCGA"
+        assert cons.score > 6.0d  && cons.score < 7.0d
+    } 
+    
+    @Test
+    void testAllDifferent() {
+        def cons = new Consensus([
+            "ATCGCGA",
+            "TCACGCT",
+            "CGTTATG",
+            "GAGATAC",
+        ].reverse())
+        cons.build()
+        assert cons.bases == "AAAAAAA"
+        assert cons.score == 0.0d
+    }  
+    
+    @Test
+    void testRead() {
+        def cons = new Consensus([
+            "CTGTCCCTGCCCCACCTCCACCCCCGCCACCTCCCCCACCTGCCACCCCTGTGACCCCGGCCCCCGTGCCTCCCTTCGAGAAGCAAGGAGGAAAGGACAAGGAAGACAAGCAGACATTCCAAGTCACAGACTGTCGAAGTTTGGTCAAAAC",
+            "CTGTCCCTGCCCCACCTCCACCCCCGCCCCCACCCCCACCTGCCACCCCTGTGACCCCGGCCCCCGTGCCTCCCTTCGAGAAGCAAGGAGAAAAGGACAAGGAAGACAAGCAGACATTCCAAGTCACAGACTGTCGAAGTTTGGTCAAAAC",
+            "CTGTCCCTGCCCCACCTCCCCCCCCGCCCCCACCCCCACCTGCCACCCCTGTGACCCCGGCCCCCGTGCCTCCCTTCGAGAAGCAAGGAGAAAAGGACAAGGAAGACAAGCAGACATTCCAAGTCACAGACTGTCGAAGTTTGG",
+        ])
+        cons.build()
+        assert cons.bases.startsWith("CTGTCCCTGCCCCACCTCCACCCCCGCCCCCACCCCCACCTGCCACCCCTGTGACCCCGGCCCCCGTGCCTCCCTTCGAGAAGCAAGGAGAAAAGGAC")
+        assert cons.score > 10.0d
+    }
+    
+    @Test
+    void testPerformance() {
+        
+        long timeMs = Utils.timeMs {
+            for(i in 0..1000) {
+                def cons = new Consensus([
+                    "ATCGCGA"*2,
+                    "ATCGCGA"*2,
+                    "ATCGCGT"*2,
+                    "ATCGCGT"*2,
+                ])
+                cons.build()
+            }
+        }
+        
+        assert timeMs < 10000
+        
+        println "Average time = " + (timeMs / 1000.0d)
+    }
+}
