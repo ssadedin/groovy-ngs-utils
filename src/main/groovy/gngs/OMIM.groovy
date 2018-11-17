4fcd6167550863a671ee8ed5e3476654a64ec3b6
Simon Sadedin <ssadedin@gmail.com>
2018-11-17 21:05:47 +1100

    adjust OMIM parsing to genemap2 format

--- src/main/groovy/gngs/OMIM.groovy
+++ src/main/groovy/gngs/OMIM.groovy
@@ -1,3 +1,4 @@
+package gngs
 /*
  *  Groovy NGS Utils - Some simple utilites for processing Next Generation Sequencing data.
  *
@@ -18,6 +19,7 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 import groovy.transform.CompileStatic;
+import java.util.regex.Pattern
 
 /**
  * Parser for OMIM genemap files
@@ -31,27 +33,30 @@ class OMIM {
     
     List<String> disorders = []
     
+    Map fields 
+    
     public static List GENEMAP2_FIELDS = [
-                "Numbering_System",
-                "Month_Entered",
-                "Day",
-                "Year",
-                "CytoLocation",
-                "Gene",
-                "Status",
-                "Title",
-                "Title2",
-                "MIMNumber",
-                "Method",
-                "Comments",
-                "Comments2",
-                "Disorders",
-                "Disorders2",
-                "Disorders3",
-                "Mouse",
-                "Reference"
-                ]
+        "Chromosome",
+        "start",
+        "End",
+        "cytoband",
+        "cytoloc",
+        "MimNo",
+        "Genes",
+        "GeneName",
+        "Symbol",
+        "EntrezID",
+        "EnsemblGeneID",
+        "Comments",
+        "Phenotypes",
+        "MouseGene"
+    ]
+    
+    final static Pattern TAB = ~/\t/
 
+                
+    // # Chromosome Genomic Position Start  Genomic Position End    Cyto Location   Computed Cyto Location  Mim Number  Gene Symbols    Gene Name   Approved Symbol Entrez Gene ID  Ensembl Gene ID Comments    Phenotypes  Mouse Gene Symbol/ID                
+                
     //@CompileStatic
     static Map<String,OMIM> parse(String fileName) {
         
@@ -61,18 +66,31 @@ class OMIM {
         int geneCount = 0
         new File(fileName).eachLine { String line ->
             
-            Map<String,String> entry = [GENEMAP2_FIELDS,line.split("\\|")].transpose().collectEntries()
+            if(line.startsWith('#'))
+                return
             
-            List disorders = ((entry.Disorders?.split(";"))?:[]) as List
-            entry.Gene.split(",")*.trim().each { gene ->
-                OMIM omim = new OMIM(gene:gene)
+            String [] parts = OMIM.TAB.split(line)
+            Map<String,String> entry = [OMIM.GENEMAP2_FIELDS,parts].transpose().collectEntries()
+            
+//            println parts
+//            println GENEMAP2_FIELDS
+//            println entry
+//            println "-" * 100
+            
+//            System.exit(0)
+            
+            List disorders = ((entry.Phenotypes?.tokenize(";"))?:[]) as List
+            
+            
+            List genes = entry.Genes?.tokenize(",")?:[]
+            genes*.trim()?.each { gene ->
+                OMIM omim = new OMIM(gene:gene, fields:entry)
                 omim.disorders = disorders
                 result[gene] = omim
-                ++geneCount
             }
             ++recordCount
         }
-        System.err.println "Loaded $recordCount records associated with $geneCount genes"
+        System.err.println "Loaded $recordCount records associated with ${result.size()} genes"
         
         return result
     }
