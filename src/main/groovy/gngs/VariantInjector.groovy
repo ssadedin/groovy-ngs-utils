fbb47d1ec49bc50121661540a8280e419726db1a
Simon Sadedin <ssadedin@gmail.com>
2018-12-15 06:59:09 +1100

    use Variant.applyTo instead of custom mutation code

--- src/main/groovy/gngs/VariantInjector.groovy
+++ src/main/groovy/gngs/VariantInjector.groovy
@@ -1,6 +1,26 @@
+/*
+ *  Groovy NGS Utils - Some simple utilites for processing Next Generation Sequencing data.
+ *
+ *  Copyright (C) 2013 Simon Sadedin, ssadedin<at>gmail.com
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package gngs
 
 import groovy.transform.CompileStatic
+import groovy.util.logging.Log
 
 /**
  * Injects a variant into a FASTQ read based on recognising the sequence corresponding to 
@@ -8,6 +28,7 @@ import groovy.transform.CompileStatic
  * 
  * @author Simon Sadedin
  */
+@Log
 class VariantInjector {
     
     FASTA reference
@@ -30,6 +51,8 @@ class VariantInjector {
     
     double alleleFraction = 0.5
     
+    boolean debugMode = false
+    
     public VariantInjector(FASTA reference, Variant variant) {
         super();
         this.reference = reference;
@@ -51,21 +74,7 @@ class VariantInjector {
      */
     @CompileStatic
     void computeReplacementBases() {
-        Allele allele = variant.alleles[0]
-        if(allele.type == 'SNP') {
-            replacementBases = referenceBases[0..9] + variant.alt + referenceBases[(10+variant.alt.size())..-1]
-        }
-        else
-        if(allele.type == 'DEL')
-        {
-            replacementBases = referenceBases[0..9] + variant.alt + referenceBases[(11+variant.size())..-1]
-            
-        }
-        else
-        if(allele.type == 'INS') {
-            replacementBases = referenceBases[0..9] + variant.alt + referenceBases[(10+variant.size())..-1]
-        }
-        
+        this.replacementBases = variant.applyTo(referenceBases,11)
         this.replacementBasesRC= FASTA.reverseComplement(replacementBases)
     }
     
@@ -80,34 +89,63 @@ class VariantInjector {
     void inject(FASTQRead r1, FASTQRead r2) {
         
         // We try to always inject into both reads or not at all
+        // Note we don't even attempt to inject into R1 RC if we injected into R1
         if(inject(r1, referenceBases, replacementBases)) {
             inject(r2, referenceBasesRC, replacementBasesRC, false)
+            renameRead(r1,r2)
         }
-        else // not we don't even attempt to inject into R1 RC if we injected into R1
+        else 
         if(inject(r2, referenceBases, replacementBases)) {
             inject(r1, referenceBasesRC, replacementBasesRC, false)
+            renameRead(r1,r2)
         }
+        else 
+        if(inject(r2, referenceBasesRC, replacementBasesRC)) {
+            inject(r1, referenceBases, replacementBases, false)
+            renameRead(r1,r2)
+        }    
+        else 
+        if(inject(r1, referenceBasesRC, replacementBasesRC)) {
+            inject(r2, referenceBases, replacementBases, false)
+            renameRead(r1,r2)
+        }        
     }
     
     @CompileStatic
+    void renameRead(FASTQRead r1, FASTQRead r2) {
+        String suffix =":SIM_${variant.chr}_${variant.pos}_${variant.ref}_${variant.ref}"
+        r1.name = r1.name + suffix
+        r2.name = r2.name + suffix
+    }
+  
+    
+    @CompileStatic
     boolean inject(FASTQRead r, String refBases, String repBases, boolean random=true) {
-        int index = r.bases.indexOf(referenceBases)
-        if(index<0) 
+        int index = r.bases.indexOf(refBases)
+        if(index<0) {
+            if(debugMode) {
+                log.info "No match of $refBases to read $r : $r.bases"
+                log.info "Alignment:\n" +Align.global(refBases, r.bases).profile.toString()
+            }
             return false
+        }
         
         ++total
-        if(random && Math.random()>alleleFraction)
+        if(random && Math.random()>alleleFraction && !debugMode)
             return false
             
         ++nReplaced
-        r.bases = r.bases.replace(referenceBases,replacementBases)
+        r.bases = r.bases.replace(refBases,repBases)
         if(r.bases.size() < r.quals.size())
             r.quals = r.quals.substring(0, r.bases.size())
         else
         if(r.bases.size() > r.quals.size()) {
             r.bases = r.bases.substring(0, r.quals.size())
         }
-        
-        r.name = r.name + ":SIM_${variant.chr}_${variant.pos}_${variant.ref}_${variant.ref}"
+       
+        if(debugMode)
+            log.info "injection to read $r : $r.bases\n" + Align.global(refBases, repBases).profile
+            
+        return true
     }
 }
