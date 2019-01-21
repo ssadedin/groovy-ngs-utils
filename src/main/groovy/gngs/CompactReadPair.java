88c24e92c22a2e313873ff0a8b35630a56757168
Simon Sadedin <ssadedin@gmail.com>
2019-01-21 16:11:43 +1100

    add more compression strategies to CompactReadPair

--- src/main/groovy/gngs/CompactReadPair.java
+++ src/main/groovy/gngs/CompactReadPair.java
@@ -1,9 +1,13 @@
 package gngs;
 
-import groovy.transform.CompileStatic;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
+import org.xerial.snappy.Snappy;
+
 import htsjdk.samtools.SAMRecord;
 import htsjdk.samtools.SAMUtils;
-import htsjdk.samtools.cram.encoding.readfeatures.BaseQualityScore;
 import htsjdk.samtools.util.SequenceUtil;
 
 public class CompactReadPair implements ReadPair {
@@ -16,9 +20,118 @@ public class CompactReadPair implements ReadPair {
     
     public int r2AlignmentStart;
     
-    public byte [] basesAndQuals;
+    public byte [][] compressedBases;
     
-    public CompactReadPair(SAMRecord read) {
+    short readLength;
+    
+    public static interface ReadCompressor {
+        byte [][] compress(byte[] bases, byte[] quals) throws IOException;
+        byte [][] expand(byte[][] basesAndQuals) throws IOException;
+    }
+    
+    
+    static class SnappyReadCompressor implements ReadCompressor {
+
+        @Override
+        public byte[][] compress(final byte[] bases, final byte [] quals) throws IOException {
+            return new byte[][] { Snappy.compress(bases), Snappy.compress(quals) };
+        }
+
+        @Override
+        public byte[][] expand(byte[][] basesAndQuals) throws IOException {
+            return new byte[][]{Snappy.uncompress(basesAndQuals[0]), Snappy.uncompress(basesAndQuals[1])};
+        }
+    }
+    
+    final static class SnappyCompactorHybridCompressor implements ReadCompressor {
+
+        @Override
+        public byte[][] compress(byte[] bases, byte[] quals) throws IOException {
+            return new byte[][] { BaseCompactor.compact(bases), Snappy.compress(quals) };
+        }
+
+        @Override
+        public byte[][] expand(byte[][] basesAndQuals) throws IOException {
+            return new byte[][]{BaseCompactor.expand(basesAndQuals[0]), Snappy.uncompress(basesAndQuals[1])};
+        }
+    } 
+    
+    
+    final static class CompactorCompressor implements ReadCompressor {
+
+        @Override
+        public byte[][] compress(final byte[] bases, final byte[] quals) throws IOException {
+            return new byte[][] { BaseCompactor.compact(bases), QualCompactor.compact(quals) };
+        }
+
+        @Override
+        public byte[][] expand(final byte[][] basesAndQuals) throws IOException {
+            return new byte[][]{BaseCompactor.expand(basesAndQuals[0]), QualCompactor.expand(basesAndQuals[1])};
+        }
+    } 
+    
+    final static class SharedArrayCompressor implements ReadCompressor {
+
+        @Override
+        public byte[][] compress(final byte[] bases, final byte[] quals) throws IOException {
+            final int readLength = (short)bases.length;
+            byte [] basesAndQuals = new byte[readLength*2];
+            System.arraycopy(bases,0,basesAndQuals,0,readLength);
+            System.arraycopy(quals,0,basesAndQuals,readLength,readLength);
+            return new byte[][] { basesAndQuals };
+        }
+
+        @Override
+        public byte[][] expand(final byte[][] basesAndQuals) throws IOException {
+            final byte [] data = basesAndQuals[0];
+            final int readLength = data.length>>1;
+            final byte[][] results = new byte[][] {
+                new byte[readLength],
+                new byte[readLength]
+            };
+            
+            System.arraycopy(data, 0, results[0], 0, readLength);
+            System.arraycopy(data, readLength, results[1], 0, readLength);            
+            
+            return results;
+        }
+    }    
+    
+    static AtomicLong memoryUsage = new AtomicLong(0);
+    
+    static AtomicLong currentCount = new AtomicLong(0);
+    
+    public static SummaryStatistics memoryStats  = new SummaryStatistics();
+    
+    static ReadCompressor initCompressor() {
+        String compressorType = System.getProperty("bazam.compressor", "snappyhybrid");
+        if(compressorType.equals("snappyhybrid")) {
+            return new SnappyCompactorHybridCompressor();
+        }
+        else
+        if(compressorType.equals("gngscompactor")) {
+            return new CompactorCompressor();
+        }
+        else
+        if(compressorType.equals("snappy")) {
+            return new SnappyReadCompressor();
+        }
+        else
+        if(compressorType.equals("none")) {        
+            return new SharedArrayCompressor();
+        }
+        else {
+            throw new IllegalStateException("bazam.compressor property was set to invalid value " + compressorType + " please choose a valid type, eg: gngscompactor");
+        }
+    }
+    
+    final static ReadCompressor compressor = initCompressor();
+    
+//    final static ReadCompressor compressor = new SnappyReadCompressor();
+//    final static ReadCompressor compressor = new CompactorCompressor();
+//    final static ReadCompressor compressor = new SharedArrayCompressor();
+    
+    public CompactReadPair(SAMRecord read) throws IOException {
         // Note that picard either uses a common string from the sequence dictionary for
         // all reads OR interns the string, so there isn't any point trying that here
         r1ReferenceName = read.getReferenceName();
@@ -28,15 +141,30 @@ public class CompactReadPair implements ReadPair {
         r1AlignmentStart = read.getAlignmentStart();
         r2AlignmentStart = read.getMateAlignmentStart();
         
-        int readLength = read.getReadLength();
-        basesAndQuals = new byte[readLength*2];
+        readLength = (short)read.getReadLength();
+//        basesAndQuals = new byte[readLength*2];
         
-        System.arraycopy(read.getReadBases(),0,basesAndQuals,0,readLength);
-        System.arraycopy(read.getBaseQualities(),0,basesAndQuals,readLength,readLength);
+//        System.arraycopy(read.getReadBases(),0,basesAndQuals,0,readLength);
+//        System.arraycopy(read.getBaseQualities(),0,basesAndQuals,readLength,readLength);
+        
+        // compressedBases = Snappy.compress(read.getReadBases());
+        
+        compressedBases = compressor.compress(read.getReadBases(), read.getBaseQualities());
+        
+        long mem = compressedBases[0].length;
+        if(compressedBases.length>1)
+            mem += compressedBases[1].length;
+        
+        memoryStats.addValue(memoryUsage.addAndGet(mem));
+        
+        currentCount.incrementAndGet();
+        
+//        System.out.println("B: " + compressedBases.length);
+//        System.out.println("Q: " + compressedQuals.length);
    }
     
    public int getReadLength() {
-       return basesAndQuals.length >> 1;
+       return (int)readLength;
    }
     
    public boolean isChimeric() {
@@ -61,7 +189,7 @@ public class CompactReadPair implements ReadPair {
        return true;
    }
    
-   public void appendTo(StringBuilder r1Out, StringBuilder r2Out, SAMRecord r2, boolean addPosition) {
+   public void appendTo(StringBuilder r1Out, StringBuilder r2Out, SAMRecord r2, boolean addPosition) throws IOException {
        
        // The logic here is complex
        // Read bases are all passed in relative to the forward strand of the reference
@@ -94,7 +222,6 @@ public class CompactReadPair implements ReadPair {
        // <-R2 <-R1
        
         final String r1Name = r2.getReadName();
-        final int readLength = basesAndQuals.length >> 1;
         
 //        StringBuilder b1 = r2.getFirstOfPairFlag() ? r1Out : r2Out;
 //        StringBuilder b2 = r2.getFirstOfPairFlag() ? r2Out : r1Out;
@@ -127,7 +254,10 @@ public class CompactReadPair implements ReadPair {
         // if it's R2 and aligned to pos strand, then we sh
         final boolean flipR1 = r2.getMateNegativeStrandFlag();
         
-        final byte [] basesAndQuals = this.basesAndQuals;
+        byte [][] expanded = compressor.expand(this.compressedBases);
+        byte [] decompressedBases = expanded[0];
+        byte [] decompressedQuals = expanded[1];
+        
         b1.append('@');
         b1.append(r1Name);
         if(nameSuffix != null) { 
@@ -135,20 +265,21 @@ public class CompactReadPair implements ReadPair {
         }        
         b1.append(" 1:N:0:1\n");
         if(flipR1)
-            SequenceUtil.reverseComplement(basesAndQuals, 0, readLength);
-        b1.append(new String(basesAndQuals, 0, readLength));
+            SequenceUtil.reverseComplement(decompressedBases);
+        
+        b1.append(new String(decompressedBases, 0, readLength));
         b1.append("\n+\n");
         
         final int twoRL = readLength*2;
         
         if(flipR1) { // read is aligned complemented, we have to turn it back
-            for(int i=twoRL-1; i>=readLength; --i) {
-                b1.append(SAMUtils.phredToFastq(basesAndQuals[i]));
+            for(int i=readLength-1; i>=0; --i) {
+                b1.append(SAMUtils.phredToFastq(decompressedQuals[i]));
             }  
         }
         else
-        for(int i=readLength; i<twoRL; ++i) {
-            b1.append(SAMUtils.phredToFastq(basesAndQuals[i]));
+        for(int i=0; i<readLength; ++i) {
+            b1.append(SAMUtils.phredToFastq(decompressedQuals[i]));
         } 
         b1.append('\n');
             
@@ -183,5 +314,12 @@ public class CompactReadPair implements ReadPair {
             }            
         }
         b2.append('\n');
+        
+        long mem = -compressedBases[0].length;
+        if(compressedBases.length>1)
+            mem -= compressedBases[1].length;
+        
+        memoryUsage.addAndGet(mem);
+        currentCount.decrementAndGet();
    }
 }
