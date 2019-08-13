3f63f56331f2f488546669efa82aa1558a43e35b
Simon Sadedin <ssadedin@gmail.com>
2019-08-13 21:38:56 +1000

    enhanced liftover tool

--- src/main/groovy/gngs/LiftOver.groovy
+++ src/main/groovy/gngs/LiftOver.groovy
@@ -3,6 +3,7 @@ package gngs
 import htsjdk.samtools.util.Interval
 import org.junit.Test
 
+import groovy.transform.CompileStatic
 import groovy.util.logging.Log
 
 @Log
@@ -17,22 +18,53 @@ class LiftOver {
 
     public Region liftOver(Region region) throws Exception {
         
-        if(chainFile == null) {
-            String toGenome = to[0].toUpperCase() + to.substring(1)
-            String urlPattern = "http://hgdownload.cse.ucsc.edu/goldenPath/${from}/liftOver/${from}To${toGenome}.over.chain.gz"
-            chainFile = new ResourceDownloader(urlPattern).download()
-        }
-        
-        if(lo == null)
-            lo = new htsjdk.samtools.liftover.LiftOver(chainFile.path)
+        init()
         
         Interval result = lo.liftOver(region as Interval)
         
+        if(!result)
+            return null
+            
         Region resultRegion = new Region(result.contig, result.start, result.end)
         resultRegion.setProps(region.expandoProperties)
         
         return resultRegion
+    }
+    
+    final static Map<String,String> genomeMappings = [
+        GRCh37: "hg19",
+        GRCh38: "hg38",
+        hg19: "hg19",
+        hg38: "hg38",
+    ]
+
+    @CompileStatic
+    private void init() {
+        if(chainFile == null) {
+            
+            String to = genomeMappings.get(to, to)
+            String from = genomeMappings.get(from, from)
+            
+            String toGenome = to[0].toUpperCase() + to.substring(1)
+            
+            String urlPattern = "https://hgdownload.cse.ucsc.edu/goldenPath/${from}/liftOver/${from}To${toGenome}.over.chain.gz"
+            chainFile = new ResourceDownloader(urlPattern).download()
+        }
+
+        if(lo == null)
+            lo = new htsjdk.samtools.liftover.LiftOver(chainFile.path)
     }    
+    
+    /**
+     * Liftover single coordinate
+     */
+    @CompileStatic
+    public int liftOver(String chr, int pos) throws Exception {
+        init()
+        Interval i = new Interval(chr, pos, pos+1)
+        return lo.liftOver(i).start
+    }
+    
     
     @Test
     void testCall() {
