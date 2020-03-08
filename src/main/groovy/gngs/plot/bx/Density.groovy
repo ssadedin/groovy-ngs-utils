b717c97a8a510d130d3909293ff22a6432015f41
Simon Sadedin <ssadedin@gmail.com>
2020-03-08 18:22:03 +1100

    fix density plots having truncated tails

--- src/main/groovy/gngs/plot/bx/Density.groovy
+++ src/main/groovy/gngs/plot/bx/Density.groovy
@@ -2,6 +2,14 @@ package gngs.plot.bx
 
 import com.twosigma.beakerx.chart.xychart.plotitem.XYGraphics
 
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class DensityPoint {
+    public double x
+    public double y
+}
+
 /**
  * Provides support for plotting of empirical PDFs in BeakerX
  * <p>
@@ -28,6 +36,10 @@ trait Density {
     
     int segments = 100
     
+    smile.stat.distribution.KernelDensity kd
+    
+    double step
+    
     static class Line extends  com.twosigma.beakerx.chart.xychart.plotitem.Line implements Density {
         Line(Map attributes) { init(attributes) }
     }
@@ -47,28 +59,81 @@ trait Density {
         this.bw = attributes.get('bw',bw).toDouble()
         this.segments = attributes.get('segments',segments).toInteger()
         
-        attributes.each { k,v -> if(!(k in DENSITY_ATTRIBUTES)) this[k] = v; }
+        attributes.each { k,v -> if(!(k in DENSITY_ATTRIBUTES)) this.properties.put(k, v); }
         
         if(!('data' in attributes)) 
-            throw new IllegalArgumentException('Please provide a data attribute with values to disply')
+            throw new IllegalArgumentException('Please provide a data attribute with values to display')
             
         double [] values = attributes.data as double[]
-        def kd = new smile.stat.distribution.KernelDensity(values, bw)
-        double min = attributes.data.min()
-        double max = attributes.data.max()
-        double step = (max - min) / segments
+        double min = ((List)attributes.data).min()
+        double max = ((List)attributes.data).max()
         
-        min -= step
-        max += step
-        
-        List yValues = new ArrayList(segments)
-        List xValues = new ArrayList(segments)
-        for(double value = min; value < max; value += step) {
-            yValues << kd.p(value)
-            xValues << value
-        }
-        
-        this.x = xValues
-        this.y = yValues
+        initXYValues(min, max, values)
     }    
+    
+    @CompileStatic
+    void initXYValues(double min, double max, double[] values) {
+        
+        kd = new smile.stat.distribution.KernelDensity(values, bw)
+        step = (max - min) / segments
+        
+        min -= step*2
+        max += step*2
+        
+        final double plotMin = min - step;
+        final double plotMax = max + step;
+        
+        LinkedList<DensityPoint> plotValues = new LinkedList()
+
+        double maxDensity = computeCoreValues(plotMin, plotMax, plotValues)
+
+        expandLeading(plotMin, maxDensity, plotValues)
+
+        expandTrailing(plotMax, maxDensity, plotValues)
+
+        this.x = plotValues*.x
+        this.y = plotValues*.y        
+    }
+    
+    @CompileStatic
+    double computeCoreValues(double plotMin, double plotMax, List plotValues) {
+        double maxDensity = 0.0d
+        for(double value = plotMin; value < plotMax; value += step) {
+            double density = kd.p(value)
+            plotValues <<  new DensityPoint(y:density, x:value)
+            if(density > maxDensity)
+                maxDensity = density
+        }
+        return maxDensity
+    }
+    
+    @CompileStatic
+    void expandTrailing(final double plotMax, final double maxDensity, final List plotValues) {
+        double tailing = plotMax + step
+        while(true) {
+            double density = kd.p(tailing)
+            if(density < 0.05 * maxDensity) {
+                break
+            }
+            plotValues.add(new DensityPoint(y:density, x:tailing))
+            tailing += step
+        }
+    }
+    
+    @CompileStatic
+    void expandLeading(final double plotMin, final double maxDensity, final List plotValues) {
+
+        // Problem: density plots usually spill pass the edge of the sampled data points
+        // therefore we keep steping backwards until the tail is below 5% of the max
+        // which ensures the distribution does not get too clipped at the end
+        double leading = plotMin - step
+        while(true) {
+            double density = kd.p(leading)
+            if(density < 0.05 * maxDensity) {
+                break
+            }
+            plotValues.add(0, new DensityPoint(y:density, x:leading))
+            leading -= step
+        }
+    }
  }
