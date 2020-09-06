2b08b7e89f2b1d09c10c48b769df3b3c2b068283
Simon Sadedin <ssadedin@gmail.com>
2020-09-06 17:48:44 +1000

    Support tab formatted input / output in LiftOver tool

--- src/main/groovy/gngs/tools/LiftOver.groovy
+++ src/main/groovy/gngs/tools/LiftOver.groovy
@@ -17,16 +17,22 @@ class LiftOver extends ToolBase {
     public void run() {
         gngs.LiftOver lo = new gngs.LiftOver(from: opts.from, to: opts.to)
         
+        def formatResult = opts.tabs ? {
+           println([it.chr, it.from, it.to].join("\t")) 
+        } : {
+           println(it.toString()) 
+        }
+        
         int count = 0
         if(opts.arguments().isEmpty()) {
             System.in.eachLine {  line ->
-                println lo.liftOver(new Region(line.trim()))
+                println formatResult(lo.liftOver(new Region(line.trim())))
                 ++count
             }
         }
         else
         for(String regionValue in opts.arguments()) {
-            println lo.liftOver(new Region(regionValue))
+            println formatResult(lo.liftOver(new Region(regionValue)))
                 ++count
         }
         
@@ -36,6 +42,7 @@ class LiftOver extends ToolBase {
     static void main(String[] args) {
         cli('Liftover -from <hg38|hg19> -to <hg38|g19> <region1> <region2> ...',
             'Lifts over regions provided as arguments or, if no arguments, reads from standard input', args) {
+            tabs 'Set output format to tab separated'
             from 'Genome build of the source coordinates', args: 1, required: true
             to 'Genome build of the target coordinates', args: 2, required: true
         }
