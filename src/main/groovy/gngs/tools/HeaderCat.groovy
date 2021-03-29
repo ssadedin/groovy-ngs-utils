3dec81ed71249e52cd38778ab42c39c56f3f4ac0
Simon Sadedin <ssadedin@gmail.com>
2021-03-29 22:09:59 +1100

    compilestatic of headercat

--- src/main/groovy/gngs/tools/HeaderCat.groovy
+++ src/main/groovy/gngs/tools/HeaderCat.groovy
@@ -2,26 +2,29 @@ package gngs.tools
 
 import gngs.ToolBase
 import gngs.Utils
+import groovy.transform.CompileDynamic
+import groovy.transform.CompileStatic
 import groovy.util.logging.Log
 
 @Log
+@CompileStatic
 class HeaderCat extends ToolBase {
 
     @Override
     public void run() {
         List<String> vcfPaths = opts.arguments()
-        Utils.writer(opts.o).withWriter { Writer w ->
-            def files = vcfPaths.collect {
+        Utils.writer((String)opts['o']).withWriter { Writer w ->
+            List<Reader> files = (List<Reader>)vcfPaths.collect {
                 Utils.reader(it)
             }
             
             int fileIndex = 0
             String vcfPath = vcfPaths[fileIndex]
             
-            log.info "Processing file $vcfPath (${fileIndex+1}/${vcfPaths.size()}"
+            log.info "Processing file $vcfPath (${fileIndex+1}/${vcfPaths.size()})"
 
             // For the first file cat the whole file
-            Reader r0 = files[0]
+            Reader r0 = (Reader)files[0]
             w << r0
             
             for(Reader r in files[1..-1]) {
@@ -38,6 +41,7 @@ class HeaderCat extends ToolBase {
         }
     }
     
+    @CompileDynamic
     static void main(String[] args) {
        cli('HeaderCat -o <output> <file1> [<file2>] ...', 
            'Smart concatenation of files of mixed compression and possibly containing headers',
