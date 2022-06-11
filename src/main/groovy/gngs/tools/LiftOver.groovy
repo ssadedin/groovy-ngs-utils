2c6429d3305ed070507d4136b94fe4f20944f030
Simon Sadedin <simon.sadedin@mcri.edu.au>
2022-06-12 08:41:07 +1000

    support liftover of BED file while preserving id column

--- src/main/groovy/gngs/tools/LiftOver.groovy
+++ src/main/groovy/gngs/tools/LiftOver.groovy
@@ -1,6 +1,8 @@
 package gngs.tools
 
+import gngs.BED
 import gngs.Region
+import gngs.Regions
 import gngs.ToolBase
 import groovy.util.logging.Log
 
@@ -26,6 +28,17 @@ class LiftOver extends ToolBase {
         }
         
         int count = 0
+        if(opts.bed) {
+            Regions regions = new BED(opts.bed, withExtra:true).load(withExtra:true)
+            regions.each {  region ->
+                def result = lo.liftOver(region)
+                if(result) {
+                    println([result.chr, result.from, result.to, region.extra].join('\t'))
+                }
+                ++count
+            }
+        }
+        else
         if(opts.arguments().isEmpty()) {
             System.in.eachLine {  line ->
                 println formatResult(lo.liftOver(new Region(line.trim())))
@@ -44,6 +57,7 @@ class LiftOver extends ToolBase {
     static void main(String[] args) {
         cli('Liftover -from <hg38|hg19> -to <hg38|g19> <region1> <region2> ...',
             'Lifts over regions provided as arguments or, if no arguments, reads from standard input', args) {
+            bed 'Read regions from BED file <arg>', args: 1, required: false, type: File
             tabs 'Set output format to tab separated'
             from 'Genome build of the source coordinates', args: 1, required: true
             to 'Genome build of the target coordinates', args: 2, required: true
