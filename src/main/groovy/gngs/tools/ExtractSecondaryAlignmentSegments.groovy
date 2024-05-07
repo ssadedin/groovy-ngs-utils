5505f958e4e7504038330b7c7392551609b5ad53
Simon Sadedin <ssadedin@gmail.com>
2024-05-08 08:50:46 +1000

    tidy up whitespace

--- src/main/groovy/gngs/tools/ExtractSecondaryAlignmentSegments.groovy
+++ src/main/groovy/gngs/tools/ExtractSecondaryAlignmentSegments.groovy
@@ -34,7 +34,7 @@ import htsjdk.samtools.SAMTag
 @Log
 class ExtractSecondaryAlignmentSegments extends ToolBase {
 
-    
+
     /**
      * Scan reads from given BAM / region to extract those with secondary alignments
      * and break the secondary alignments into separate SASegment objects
@@ -45,59 +45,55 @@ class ExtractSecondaryAlignmentSegments extends ToolBase {
      */
     @CompileStatic
     List<List> scanReads(SAM bam, Region region) {
-        
+
         return (List<List>)bam.withIterator(region) {  i ->
             i.grep { SAMRecord r ->
                 !r.readUnmappedFlag && r.hasAttribute(SAMTag.SA)
             }.collect { o ->
-                
+
                 SAMRecord r  = (SAMRecord) o
                 String strand = r.readNegativeStrandFlag ? "-" : "+"
-                SASegment primaryAlignment = 
-                    new SASegment(chr: r.referenceName, pos: r.alignmentStart, strand: strand, cigar: r.cigarString, qual: r.mappingQuality)
+                SASegment primaryAlignment =
+                        new SASegment(chr: r.referenceName, pos: r.alignmentStart, strand: strand, cigar: r.cigarString, qual: r.mappingQuality)
 
-                List<SASegment> segs = 
-                    [primaryAlignment] +
+                List<SASegment> segs =
+                        [primaryAlignment] +
 
-                    r.getStringAttribute(SAMTag.SA)
-                     .split(";")
-                     .grep { it }
-                     *.split(",")
-                     .collect { String[] parts ->
-                        new SASegment(
-                            chr: parts[0],
-                            pos: parts[1].toInteger(),
-                            strand: parts[2],
-                            cigar: parts[3],
-                            qual: parts[4].toInteger()
-                        )
-                    }
-               return [r.readName, segs]
+                        r.getStringAttribute(SAMTag.SA)
+                        .split(";")
+                        .grep { it }
+                        *.split(",")
+                        .collect { String[] parts ->
+                            new SASegment(
+                                    chr: parts[0],
+                                    pos: parts[1].toInteger(),
+                                    strand: parts[2],
+                                    cigar: parts[3],
+                                    qual: parts[4].toInteger()
+                                    )
+                        }
+                return [r.readName, segs]
             }
         }
-
     }
 
     @Override
     public void run() {
-        
-        Region locus = new Region(opts.locus)
-        
-        log.info "Scanning locus $locus in $opts.bam"
 
+        Region locus = new Region(opts.locus)
+
+        log.info "Scanning locus $locus in $opts.bam"
         List<List> reads = scanReads(new SAM(opts.bam), locus)
-        
-        log.info "Finsihed scan: found ${reads.size()} reads"
-        
-       
+        log.info "Finished scan: found ${reads.size()} reads"
+
         List<Map> results = reads.collectMany { List item ->
-            
-           String name = item[0]
+
+            String name = item[0]
 
             return item[1].collect { SASegment seg ->
 
                 Map cigarSplit = seg.splitCigar()
-               
+
                 return [
                     "readid": name,
                     "chrom": seg.chr,
@@ -107,11 +103,10 @@ class ExtractSecondaryAlignmentSegments extends ToolBase {
                     * : cigarSplit
                 ]
             }
-            
         }
         println(new JsonBuilder(results).toPrettyString())
     }
-    
+
     static void main(String [] args) {
         cli('Extract alignment segments', args) {
             bam 'BAM File', args:1, required: true
@@ -130,9 +125,9 @@ class SASegment {
     String strand
     String cigar
     int qual
-    
+
     final static Pattern cigarSplitter = ~"([0-9]+)([MIDSH])"
-    
+
     /**
      * Compute the total query length, mapped length and offset (based on soft clips)
      * of the CIGAR string of this segment
@@ -140,9 +135,9 @@ class SASegment {
      * @return  Map with rlen, qlen and offset attributes
      */
     Map<String, Integer> splitCigar() {
-        
+
         List<String> cig =
-                cigarSplitter.matcher(this.cigar).collect { [it[1].toInteger(),it[2]] }
+                cigarSplitter.matcher(this.cigar).collect { [it[1].toInteger(), it[2]] }
 
         if(this.strand == "-")
             cig = cig.reverse()
@@ -155,7 +150,7 @@ class SASegment {
             qlen  : cig.grep { it[1] == 'M' || it[1] == 'I' }.sum { it[0] }?:0,
 
             // length of any leading clipped sequence
-            offset : cig[0][1] in ['S','H'] ? cig[0][0] : 0
+            offset : cig[0][1] in ['S', 'H'] ? cig[0][0] : 0
         ]
-     }
+    }
 }
