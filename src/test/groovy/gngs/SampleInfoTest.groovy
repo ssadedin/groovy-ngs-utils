afb9be0c6e55ccaa5c96aa4394881d3f348b0af3
Simon Sadedin <simon.sadedin@mcri.edu.au>
2025-06-25 15:26:24 +1000

    move sample info test to package

---
+++ src/test/groovy/gngs/SampleInfoTest.groovy
@@ -0,0 +1,85 @@
+package gngs
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import gngs.sample.*
+
+class SampleInfoTest {
+	
+//	static String TEST_FILE_NAME = "test_sample_info.tsv"
+	static String TEST_FILE_NAME = "samples_AGRF_002.txt"
+
+	@Test
+	public void testParse() {
+        
+		if(!new File("tests/$TEST_FILE_NAME").exists()) {
+			println "Skipping test parse because file does not exist. Please download and place '$TEST_FILE_NAME' in the tests local directory."
+			return
+		}
+		
+		Map samples = SampleInfo.parse_sample_info("tests/$TEST_FILE_NAME")
+		
+		SampleInfo s = samples["010100101"]
+		
+		println "Checking sample $s"
+		
+		assert s != null
+		
+		assert s.batch == "002"
+		assert s.target == "CS"
+		assert s.files.fastq.find { it.startsWith("../data/Sample1_Lab2_Batch1_")}
+		assert s.sex == Sex.MALE
+		assert s.ethnicity == Ethnicity.EUROPEAN
+		assert s.consanguinity == Consanguinity.SUSPECTED
+		assert s.sampleType == SampleType.NORMAL
+		assert s.dnaConcentrationNg == 30
+		assert s.dnaQuantity == 550
+		assert s.dnaQuality == 1.8f
+		assert s.dnaQuality == 1.8f
+		assert s.sequencingDates[0].toCalendar().get(Calendar.DAY_OF_MONTH) == 10
+		assert s.sequencingContact == "sequencing@melbournegenomics.org.au"
+		assert s.analysisContact == "pipeline@melbournegenomics.org.au"
+		assert s.machineIds[1] == "700890"
+		
+		s.validate()
+	}
+	
+//	@Test
+	void testNoOptionalFields() {
+		
+		Map samples = SampleInfo.parse_sample_info("tests/samples.simple.txt")
+		
+		SampleInfo s = samples["000000000"]
+		
+		println "Parsed sample $s"
+		assert s.batch == "000"
+		
+	}
+	
+	@Test
+	void testParseDate() {
+		SampleInfo s = new SampleInfo()
+		Calendar d = s.parseDate("20140320").toCalendar()
+		assert d.get(Calendar.DAY_OF_MONTH) == 20
+		assert d.get(Calendar.YEAR) == 2014
+		assert d.get(Calendar.MONTH) == 2
+	}
+    
+    @Test
+    void testSampleInfoFromFiles() {
+        def samples = SampleInfo.fromFiles(null)
+    }
+    
+    @Test
+    void testWithIdentifiers() {
+        
+        if(!new File('src/test/data/samples-with-identifiers.txt').exists())
+            return
+        
+        def headers = [
+            'Batch','Sample_ID','DNA_Tube_ID','Sex','DNA_Concentration','DNA_Volume','DNA_Quantity','DNA_Quality','DNA_Date','Cohort','Sample_Type','Fastq_Files','Prioritised_Genes','Consanguinity','Variants_File','Pedigree_File', 'Identifiers', 'Test_ID'
+        ]
+        
+        Map info = SampleInfo.parse_sample_info('src/test/data/samples-with-identifiers.txt', headers)
+    }
+}
