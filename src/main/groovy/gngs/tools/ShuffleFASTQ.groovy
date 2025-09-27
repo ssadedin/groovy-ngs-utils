6800390c8f4fd92ecc0557c72a487454fbf28e29
Simon Sadedin <simon.sadedin@mcri.edu.au>
2025-09-27 12:27:07 +1000

    initial WIP implementation of FASTQ shuffler

---
+++ src/main/groovy/gngs/tools/ShuffleFASTQ.groovy
@@ -0,0 +1,41 @@
+package gngs.tools
+
+import java.util.concurrent.LinkedBlockingDeque
+
+import gngs.FASTQ
+import gngs.FASTQRead
+import gngs.ToolBase
+import gngs.Utils
+import groovy.transform.CompileStatic
+
+class ShuffleFASTQ extends ToolBase {
+
+    @Override
+    public void run() {
+        
+        String r1 = opts.r1
+        String r2 = r1.replaceAll('_R1', '_R2')
+        
+        String o1 = r1.replaceAll('.fastq.gz$', '.shuffled.fastq.gz')
+        String o2 = r1.replaceAll('.fastq.gz$', '.shuffled.fastq.gz')
+        
+        FASTQRead [][] buffer = new FASTQRead[(int)(opts.n)]
+         writeFASTQ(buffer, r1, r2, o1, o2)
+    }
+
+
+    @CompileStatic
+    private writeFASTQ(final FASTQRead [][] buffer, String r1, String r2, String o1, String o2) {
+        Utils.withWriters([o1,o2]) {  w1, w2 ->
+            FASTQ.eachPair(r1, r2) { read1, read2 ->
+            }
+        }
+    }
+    
+    static void main(String[] args) {
+        cli('-i1 <FASTQ>', args) {
+            i1 'FASTQ R1 file to shuffle: R2 is inferred', args:1, required: true
+            n 'Number of reads to include in shuffle buffer', args:1, required: true, type: Integer
+        }
+    }
+}
